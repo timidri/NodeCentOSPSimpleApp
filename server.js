49245cb620963aa2cd607a3bd6284e5406b7c9a3
Dimitri Tischenko <dimitri.tischenko@dimitri.tischenko-C02V71QBG8WL>
2017-11-01 11:57:59 +0100

    changed message

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ var http = require('http');
 
 http.createServer(function (request, response) {
     response.writeHead(200, {'Content-Type': 'text/plain'});
-    response.end('Hello World! Your Node.js application is working!\n');
+    response.end('Hello World! Your Node.js docker application is working!\n');
 }).listen(8080);
 
 console.log('Server started');
