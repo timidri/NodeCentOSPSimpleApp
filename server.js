1c72ab7fc71425d456fceb1976f48dca256051ee
Dimitri Tischenko <dimitri.tischenko@dimitri.tischenko-C02V71QBG8WL>
2017-11-09 18:36:34 +0100

    changed port to 3000

--- server.js
+++ server.js
@@ -3,6 +3,6 @@ var http = require('http');
 http.createServer(function (request, response) {
     response.writeHead(200, {'Content-Type': 'text/plain'});
     response.end('Hello World! Your Node.js application is working!\n');
-}).listen(8080);
+}).listen(3000);
 
 console.log('Server started');
