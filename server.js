e0bbdcbfb19837afb4a9bc0dce6df0d4c478b187
Dimitri Tischenko <dimitri.tischenko@dimitri.tischenko-C02V71QBG8WL>
2017-11-16 02:38:18 +0100

    revert

--- server.js
+++ server.js
@@ -2,8 +2,7 @@ var http = require('http');
 
 http.createServer(function (request, response) {
     response.writeHead(200, {'Content-Type': 'text/plain'});
-    response.writeBody(200, {'<h1>Hello World! Your Node.js application is working!</h1>'});
-    response.end();
+    response.end('Hello World! Your Node.js application is working!\n');
 }).listen(3000);
 
 console.log('Server started');
