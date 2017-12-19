6f4a6d45e2796f73a9926e710b9cf3b04f09fed7
Dimitri Tischenko <dimitri.tischenko@dimitri.tischenko-C02V71QBG8WL>
2017-12-19 15:44:36 +0100

    changed to html

--- server.js
+++ server.js
@@ -1,8 +1,8 @@
 var http = require('http');
 
 http.createServer(function (request, response) {
-    response.writeHead(200, {'Content-Type': 'text/plain'});
-    response.end('Hello World! Your Node.js docker application is working!\n');
+    response.writeHead(200, {'Content-Type': 'text/html'});
+    response.end('<h1>Hello World! Your Node.js docker application is working!</h1>');
 }).listen(8080);
 
 console.log('Server started');
